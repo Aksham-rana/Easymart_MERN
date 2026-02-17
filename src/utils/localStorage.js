34f4866c0d52a4ad108098846265acdcc0710533
Aksham Rana <ranaaksham2611@gmail.com>
2026-02-17 22:07:01 +0530

    Fix localStorage.js - remove duplicate function declarations

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -21,33 +21,3 @@ export const removeCurrentSession =()=>{
   return localStorage.removeItem('CurrentUser')
 }
 
-
-
-
-
-export const setUser =(data)=>{
-  const existingUsers = JSON.parse(localStorage.getItem('users')) || [];
-  existingUsers.push(data);
-  localStorage.setItem('users', JSON.stringify(existingUsers));
-}
-
-export const getUser = ()=>{
-  return JSON.parse(localStorage.getItem('users')) || []
-}
-
-export const currentSession =(data)=>{
-  localStorage.setItem('CurrentUser',JSON.stringify(data))
-}
-
-export const getCurrentSession =()=>{
-  return JSON.parse(localStorage.getItem('CurrentUser'))
-}
-
-export const removeCurrentSession =()=>{
-  return localStorage.removeItem('CurrentUser')
-}
-
-
-
-
-
