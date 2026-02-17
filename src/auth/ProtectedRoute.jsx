39b5c84e92f25e46e6d92c603d42b4a7e87a3e73
Aksham Rana <ranaaksham2611@gmail.com>
2026-02-17 22:02:53 +0530

    Remove all duplicate component definitions from JSX files - final fix for Vercel deployment

--- src/auth/ProtectedRoute.jsx
+++ src/auth/ProtectedRoute.jsx
@@ -13,19 +13,4 @@ const ProtectedRoute = ({children}) => {
 }
 
 export default ProtectedRoute
-import React from 'react'
-import { getCurrentSession } from '../utils/localStorage';
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({children}) => {
-
-    const isLoggedIn =  getCurrentSession('user')
-
-    if(!isLoggedIn){
-        return <Navigate to={'/'}/>
-    }
-    return children;
-}
-
-export default ProtectedRoute
 
